f9233f4cc72ed544ea22c2c4d5e87a7d693ad698
beef-erikson <beef.erikson.studios@gmail.com>
2019-09-11 03:56:20 -0400

    Exercise1-2 finished

---
+++ Chapter1/Exercise1-2.c
@@ -0,0 +1,16 @@
+/*
+ * Exercise1-2.c
+ *
+ *  Created on: Sep 11, 2019
+ *      Author: beef
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	// This errors, as expected / intended
+	printf("Hello World\l");
+
+	return 0;
+}
