75288f5d45af7e49e3a0c0736088678c209e103a
beef-erikson <beef.erikson.studios@gmail.com>
2019-09-12 09:21:24 -0400

    Print value of EOF complete

---
+++ Chapter1/Exercise1-7/Exercise1-7.c
@@ -0,0 +1,16 @@
+/*
+ * Exercise1-7.c
+ *
+ *  Created on: Sep 12, 2019
+ *      Author: beef
+ */
+
+#include <stdio.h>
+
+/* Prints the value of EOF */
+
+int main() {
+	printf("value of EOF: %c", EOF);
+
+	return 0;
+}
