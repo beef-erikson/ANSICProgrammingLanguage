1b324483393dfb856eabb09ee59d19d5e87cced6
beef-erikson <beef.erikson.studios@gmail.com>
2019-09-12 12:40:44 -0400

    Replaces tab, backspace and backslash with their escape characters

---
+++ Chapter1/Exercise1-10/Exercise1-10.c
@@ -0,0 +1,36 @@
+/*
+ * Exercise1-10.c
+ *
+ *  Created on: Sep 12, 2019
+ *      Author: beef
+ */
+
+#include <stdio.h>
+
+/* Replaces tab, backspace and / with the escape characters instead */
+
+int main() {
+	int c;
+
+	while ((c = getchar()) != EOF) {
+		if (c == '\t') {
+			putchar('\\');
+			putchar('t');
+			c = getchar();
+		}
+		// note that backspace doesn't show in linux from input
+		if (c == '\b') {
+			putchar('\\');
+			putchar('b');
+			c = getchar();
+		}
+		if (c == '\\') {
+			putchar('\\');
+			putchar('\\');
+			c = getchar();
+		}
+		putchar(c);
+	}
+
+	return 0;
+}
