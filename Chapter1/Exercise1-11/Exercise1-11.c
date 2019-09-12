4c3b2857092253488df0dd57a54cbf9111fa5e80
beef-erikson <beef.erikson.studios@gmail.com>
2019-09-12 15:04:08 -0400

    count lines, words and characters in input

---
+++ Chapter1/Exercise1-11/Exercise1-11.c
@@ -0,0 +1,36 @@
+/*
+ * Exercise1-11.c
+ *
+ *  Created on: Sep 12, 2019
+ *      Author: beef
+ */
+
+#include <stdio.h>
+
+#define IN 	1 	// Inside a word
+#define OUT 0	// Outside a word
+
+/* Count lines, words, and characters in input */
+
+int main() {
+	int c, newline, words, characters, state;
+
+	state = OUT;
+	newline = words = characters = 0;
+
+	while ((c = getchar()) != EOF) {
+		++characters;
+		if (c == '\n')
+			++newline;
+		if (c == ' ' || c == '\n' || c == '\t')
+			state = OUT;
+		else if (state == OUT) {
+			state = IN;
+			++words;
+		}
+	}
+	printf("\nLines: %d, Words: %d, Characters: %d\n", newline, words, characters);
+
+	return 0;
+}
+
