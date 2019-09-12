c2b24ef7a605ff695979bdc3920328fbcddfd440
beef-erikson <beef.erikson.studios@gmail.com>
2019-09-12 07:31:12 -0400

    Celsius to Fahrenheit converter finished

---
+++ Chapter1/Exercise1-4/Exercise1-4.c
@@ -0,0 +1,32 @@
+/*
+ * Exercise1-4.c
+ *
+ *  Created on: Sep 12, 2019
+ *      Author: beef
+ */
+
+#include <stdio.h>
+
+/* Prints Celsius-Fahrenheit table
+ * for fahr = 0, 20 ... 300 */
+
+int main() {
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+
+	printf("Celsius to Fahrenheit conversion table\n");
+	printf("--------------------------------------\n");
+
+	while (celsius <= upper) {
+		fahr = celsius * 9.0/5.0 + 32;
+		printf("%3.0f% 8.1f\n", celsius, fahr);
+		celsius += step;
+	}
+	return 0;
+}
