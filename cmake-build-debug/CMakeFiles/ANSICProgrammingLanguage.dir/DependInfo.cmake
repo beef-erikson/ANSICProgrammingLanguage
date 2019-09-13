18767a6d3b3159585806b669bd71455c4381c2cd
beef-erikson <beef.erikson.studios@gmail.com>
2019-09-13 07:55:15 -0400

    Changed IDE to CLion. Had to comment out files not currently working on for building

---
+++ cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-1/Exercize1-1.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-1/Exercize1-1.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-10/Exercise1-10.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-10/Exercise1-10.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-11/Exercise1-11.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-11/Exercise1-11.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-12/Exercise1-12.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-12/Exercise1-12.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-13/Exercise1-13.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-13/Exercise1-13.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-14/Exercise1-14.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-14/Exercise1-14.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-15/Exercise1-15.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-15/Exercise1-15.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-2/Exercise1-2.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-2/Exercise1-2.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-3/Exercize1-3.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-3/Exercize1-3.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-4/Exercise1-4.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-4/Exercise1-4.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-5/Exercise1-5.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-5/Exercise1-5.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-6/Exercise1-6.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-6/Exercise1-6.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-7/Exercise1-7.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-7/Exercise1-7.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-8/Exercise1-8.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-8/Exercise1-8.c.o"
+  "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/Chapter1/Exercise1-9/Exercise1-9.c" "/home/beef/Desktop/CLionProjects/ANSICProgrammingLanguage/cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-9/Exercise1-9.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ANSICProgrammingLanguage.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-1/Exercize1-1.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-10/Exercise1-10.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-11/Exercise1-11.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-12/Exercise1-12.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-13/Exercise1-13.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-14/Exercise1-14.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-2/Exercise1-2.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-3/Exercize1-3.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-4/Exercise1-4.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-5/Exercise1-5.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-6/Exercise1-6.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-7/Exercise1-7.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-8/Exercise1-8.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-9/Exercise1-9.c.o"
+  "CMakeFiles/ANSICProgrammingLanguage.dir/Chapter1/Exercise1-15/Exercise1-15.c.o"
+  "ANSICProgrammingLanguage.pdb"
+  "ANSICProgrammingLanguage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ANSICProgrammingLanguage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
